800e92d17233878eb016c5a97c3db864e078ce70
amirhassani34 <amirhassani34@gmail.com>
2026-08-31 17:18:56 +0500

    updated main.tf

--- infra/env/dev/backend.tf
+++ infra/env/dev/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "zen-pharma-terraform-state-nullbyte"
-    key    = "env/dev/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket       = "zen-pharma-terraform-state-nullbyte"
+    key          = "env/dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
-    
+
   }
 }
