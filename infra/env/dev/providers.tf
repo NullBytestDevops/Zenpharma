fbf57a8551ae746bc6aef0634a3bc1fd671c5118
amirhassani34 <amirhassani34@gmail.com>
2026-09-01 13:44:26 +0500

    feat: add EKS, ECR, RDS, secrets-manager modules and wire dev environment

--- infra/env/dev/providers.tf
+++ infra/env/dev/providers.tf
@@ -6,17 +6,35 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-
   default_tags {
     tags = {
       Project   = "pharma"
       Env       = "dev"
       ManagedBy = "Terraform"
     }
+  }
+}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
